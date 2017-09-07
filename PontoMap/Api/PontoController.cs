ff11a4d9dbee0d81f74c4d92d927dbfe0956e62b
cadudragon <carlos8eduardo@gmail.com>
2017-09-07 16:40:22 -0300

    Implementação - Instalação dos pacotes para funcionamento da webApi com autenticação via token usando Owin

---
+++ PontoMap/Api/PontoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PontoMap.Api
+{
+    public class PontoController : ApiController
+    {
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/time")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Now server time is: " + DateTime.Now);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/autenticado")]
+        public IHttpActionResult autenticado()
+        {
+            return Ok("Krai de asa, Now server time is: " + DateTime.Now);
+        }
+    }
+}
---
+++ PontoMap/AuthorizationServerProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using PontoMap.BOs;
+using PontoMap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PontoMap
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Valida as requisições posteriores ao login 
+        /// Fonte: https://msdn.microsoft.com/en-us/library/microsoft.owin.security.oauth.oauthauthorizationserverprovider.validateclientauthentication(v=vs.113).aspx
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            await Task.FromResult(context.Validated());
+        }
+
+        /// <summary>
+        /// Metodo que é acionado no login da webApi pela uri/token
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            Usuario user = new Usuario { CdPassword = context.Password, DsEmail = context.UserName };
+            new UsuarioBo().Login(user);
+
+            if (user.Status == 1)
+            {
+
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", user.NmUsuario));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "user name"));
+                await Task.FromResult(context.Validated(identity));
+            }
+            else
+            {
+                context.SetError("Credenciais invalidas", "Combinação de Usuário e senha incorreta.");
+                return;
+            }
+        }
+    }
+}
